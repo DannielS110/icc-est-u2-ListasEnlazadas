89955715000d78b129d7a7cf162a11e8267f77d0
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2025-06-11 17:54:12 -0500

    Version final

--- src/App.java
+++ src/App.java
@@ -1,17 +1,39 @@
 import Controller.MenuController;
+import Controller.ContactManager;
 import Model.Contact;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+        ContactManager cm = new ContactManager();
+        
+        Contact c1 = new Contact("Juan", "123");
+        Contact c2 = new Contact("Ana", "456"); 
+        Contact c3 = new Contact("Pedro", "789");
+        
+        cm.addContact(c1);
+        cm.addContact(c2);
+        cm.addContact(c3);
+        
+        System.out.println("=== Lista inicial ===");
+        cm.printList();
+        
+        System.out.println("\n=== Probando búsqueda ===");
+        Contact found = cm.findContactByName("juan"); 
+        
+        if (found != null) {
+            System.out.println("Encontrado: " + found);
+        } else {
+            System.out.println("No encontrado");
+        }
+        
+        System.out.println("\n=== Probando eliminación ===");
+        cm.deleteContactByName("ANA"); 
+        
+        System.out.println("\n=== Lista después de eliminar ===");
+        cm.printList();
+        
         MenuController menu = new MenuController();
-        
-        menu.getContactManager().addContact(new Contact("Juan Perez", "123-456-7890"));
-        menu.getContactManager().addContact(new Contact("Maria Garcia", "098-765-4321"));
-        menu.getContactManager().addContact(new Contact("Carlos Lopez", "555-123-4567"));
-        
-        System.out.println("Sistema iniciado con contactos de ejemplo:");
-        menu.getContactManager().printList();
-        
         menu.showMenu();
     }
 }
