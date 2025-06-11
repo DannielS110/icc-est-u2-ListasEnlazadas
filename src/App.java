cccee6b7ee304edb0ad9e15574aab8985be56433
JustinLucero07 <>
2025-06-10 19:40:44 -0500

    Implementacion del App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import Controller.MenuController;
+import Model.Contact;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        MenuController menu = new MenuController();
+        
+        menu.getContactManager().addContact(new Contact("Juan Perez", "123-456-7890"));
+        menu.getContactManager().addContact(new Contact("Maria Garcia", "098-765-4321"));
+        menu.getContactManager().addContact(new Contact("Carlos Lopez", "555-123-4567"));
+        
+        System.out.println("Sistema iniciado con contactos de ejemplo:");
+        menu.getContactManager().printList();
+        
+        menu.showMenu();
     }
 }
