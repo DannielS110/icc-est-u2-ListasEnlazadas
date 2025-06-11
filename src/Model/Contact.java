79a8d526740083bb7434a33e9aa65ba04e819dc8
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2025-06-11 17:41:41 -0500

    agregacion del metodo equals

--- src/Model/Contact.java
+++ src/Model/Contact.java
@@ -9,6 +9,11 @@ public class Contact {
         this.phone = phone;
     }
     
+    public Contact(String name) {
+        this.name = name;
+        this.phone = null;
+    }
+    
     public String getName() {
         return name;
     }
@@ -32,9 +37,29 @@ public class Contact {
     
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Contact contact = (Contact) obj;
-        return name.equalsIgnoreCase(contact.name);
+        if (this == obj) {
+            return true;
+        }
+        
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        Contact other = (Contact) obj;
+        
+        if (this.name == null) {
+            return other.name == null;
+        }
+        
+        return this.name.equalsIgnoreCase(other.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        return name == null ? 0 : name.toLowerCase().hashCode();
     }
 }
