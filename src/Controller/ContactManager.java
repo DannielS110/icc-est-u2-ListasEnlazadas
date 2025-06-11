d75905fe9ae7ec2c29901e0e6dbff9c3ebd81b11
JustinLucero07 <>
2025-06-10 19:37:38 -0500

    Implementado codigo en la carpeta Controller

--- src/Controller/ContactManager.java
+++ src/Controller/ContactManager.java
@@ -1,5 +1,55 @@
 package Controller;
 
+import Model.*;
+
 public class ContactManager {
+    private LinkedList<Contact> contacts;
     
+    public ContactManager() {
+        this.contacts = new LinkedList<>();
+    }
+    
+    public void addContact(Contact contact) {
+        if (findContactByName(contact.getName()) != null) {
+            System.out.println("El contacto ya existe.");
+            return;
+        }
+        contacts.appendToTail(contact);
+        System.out.println("Contacto agregado exitosamente.");
+    }
+    
+    public Contact findContactByName(String name) {
+        Node<Contact> current = contacts.getHead();
+        while (current != null) {
+            if (current.getValue().getName().equalsIgnoreCase(name)) {
+                return current.getValue();
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+    
+    public void deleteContactByName(String name) {
+        Contact contactToDelete = findContactByName(name);
+        if (contactToDelete != null) {
+            contacts.deleteByValue(contactToDelete);
+            System.out.println("Contacto eliminado exitosamente.");
+        } else {
+            System.out.println("Contacto no encontrado.");
+        }
+    }
+    
+    public void printList() {
+        if (contacts.isEmpty()) {
+            System.out.println("No hay contactos en la lista.");
+        } else {
+            System.out.println("\n=== LISTA DE CONTACTOS ===");
+            contacts.print();
+            System.out.println("Total de contactos: " + contacts.getSize());
+        }
+    }
+    
+    public LinkedList<Contact> getContacts() {
+        return contacts;
+    }
 }
--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -1,5 +1,84 @@
 package Controller;
 
+import Model.Contact;
+import View.ConsoleView;
+
 public class MenuController {
+    private ContactManager contactManager;
+    private ConsoleView consoleView;
     
+    public MenuController() {
+        this.contactManager = new ContactManager();
+        this.consoleView = new ConsoleView();
+    }
+    
+    public void showMenu() {
+        boolean running = true;
+        
+        while (running) {
+            consoleView.displayMenu();
+            String option = consoleView.getInput("");
+            
+            switch (option) {
+                case "1":
+                    addContact();
+                    break;
+                case "2":
+                    findContact();
+                    break;
+                case "3":
+                    deleteContact();
+                    break;
+                case "4":
+                    printList();
+                    break;
+                case "5":
+                    running = false;
+                    consoleView.showMessage("¡Gracias por usar el sistema!");
+                    break;
+                default:
+                    consoleView.showMessage("Opción no válida. Intente nuevamente.");
+            }
+        }
+        consoleView.close();
+    }
+    
+    private void addContact() {
+        consoleView.showMessage("\n=== AGREGAR CONTACTO ===");
+        String name = consoleView.getInput("Ingrese el nombre: ");
+        String phone = consoleView.getInput("Ingrese el teléfono: ");
+        
+        if (!name.isEmpty() && !phone.isEmpty()) {
+            Contact newContact = new Contact(name, phone);
+            contactManager.addContact(newContact);
+        } else {
+            consoleView.showMessage("Error: El nombre y teléfono no pueden estar vacíos.");
+        }
+    }
+    
+    private void findContact() {
+        consoleView.showMessage("\n=== BUSCAR CONTACTO ===");
+        String name = consoleView.getInput("Ingrese el nombre a buscar: ");
+        
+        Contact contact = contactManager.findContactByName(name);
+        if (contact != null) {
+            consoleView.showMessage("Contacto encontrado: " + contact.toString());
+        } else {
+            consoleView.showMessage("Contacto no encontrado.");
+        }
+    }
+    
+    private void deleteContact() {
+        consoleView.showMessage("\n=== ELIMINAR CONTACTO ===");
+        String name = consoleView.getInput("Ingrese el nombre del contacto a eliminar: ");
+        contactManager.deleteContactByName(name);
+    }
+    
+    private void printList() {
+        contactManager.printList();
+    }
+    
+    public ContactManager getContactManager() {
+        return contactManager;
+    }
 }
